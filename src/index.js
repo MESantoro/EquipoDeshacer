d6f0ceb39b5db997bc9f8708715a37e9fa6f3559
MIeres_K <mieres.k@gmail.com>
2023-09-20 20:40:41 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 //Setings (Puerta de Conexion)
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exphbs({
+app.engine('.hbs', exphbs.engine({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
