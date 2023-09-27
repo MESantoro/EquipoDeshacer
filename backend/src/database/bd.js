6d4877452cc333f2f5b8437737fe5674cf02b1bb
martinsanto <martinsantoro81@gmail.com>
2023-09-27 20:05:35 -0300

    Modificacion de la estructura por falla de orden

---
+++ backend/src/database/bd.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql');
+
+const mysqlConeccion= mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'estadosycuentas' //HAY Q PONER BD CUENTAS
+});
+
+mysqlConeccion.connect(function(err){
+    if(err){
+        console.log('EL ERROR DE CONEXIÓN ES: ', err)
+        return;
+    }else{
+        console.log('¡¡¡MIRÁ VOS CHE... METANLE FICHA QUE LLEGAN...!!!')
+    }
+})
+
+module.exports=mysqlConeccion;
+
+/* const mysql = require('mysql'); //ANDA
+
+const {bd} = require('.keys'); //ANDA
+
+const pool = mysql.createPool(bd);
+
+pool.getConnection(err, conection) => {
+    if (err.code === 'PROTOCOL_CONNECTION');
+}; */
---
+++ backend/src/keys.js
@@ -0,0 +1,14 @@
+//Lave para conexion de la Base de Datos (host, usuario, calve y base)
+
+
+
+/* module.exports = {
+
+    bd: {
+        host:'localhost',
+        user:'root',
+        password:'root',
+        database:'bd.links' //HAY QUE CAMBIAR A ESTADO DE CUENTAS
+    }
+
+}; */
